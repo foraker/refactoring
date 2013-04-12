be07612c5804b13e8ccdedc75370056973b25fd4
Raman Sinha <raman.sinha@returnpath.net>
2013-04-11 19:07:24 -0600

    initial stab
    Change-Id: I5dba4aba4e87aef9f701c5c74cb51f63f4359bc8

--- april_2013/insight_lookup.rb
+++ april_2013/insight_lookup.rb
@@ -18,44 +18,68 @@ class InsightLookup
   end
 
   def analyze
-    @target_score.nil? ? text_without_target : text_with_target
+    #@target_score.nil? ? text_without_target : text_with_target
+    #removed ternary operator so easier to read
+    #made negative easier to read by removing check for nil
+    if @target_score
+      text_with_target
+    else
+      text_without_target
+    end
   end
 
   private
 
   def text_without_target
-    @applicant_score > 60 ? INSIGHTS[:applicant_overdeveloped_text] : INSIGHTS[:applicant_underdeveloped_text]
-  end
-
-  def text_with_target
-    if @applicant_score < 40 # underdeveloped
-      if @target_score < 40
-        if @applicant_score < @target_score
-          return INSIGHTS[:target_low_applicant_more_underdeveloped_text]
-        elsif @applicant_score > @target_score
-          return INSIGHTS[:target_low_applicant_less_underdeveloped_text]
-        else
-          return INSIGHTS[:target_low_applicant_less_underdeveloped_text]
-        end
-      elsif @target_score > 60
-        return INSIGHTS[:target_high_applicant_underdeveloped_text]
-      else
-        return INSIGHTS[:target_general_applicant_underdeveloped_text]
-      end
-    elsif @applicant_score > 60 #overdeveloped
-      if @target_score < 40
-        return INSIGHTS[:target_low_applicant_overdeveloped_text]
-      elsif @target_score > 60
-        if @applicant_score > @target_score
-          return INSIGHTS[:target_high_applicant_more_overdeveloped_text]
-        elsif @applicant_score < @target_score
-          return INSIGHTS[:target_high_applicant_less_overdeveloped_text]
-        else
-          return INSIGHTS[:target_high_applicant_less_overdeveloped_text]
-        end
-      else
-        return INSIGHTS[:target_general_applicant_overdeveloped_text]
-      end
+    #  @applicant_score > 60 ? INSIGHTS[:applicant_overdeveloped_text] : INSIGHTS[:applicant_underdeveloped_text]
+    if @applicant_score > 60
+      INSIGHTS[:applicant_overdeveloped_text]
+    else
+      INSIGHTS[:applicant_underdeveloped_text]
     end
   end
-end
+  
+  
+  def text_with_target
+    case @applicant_score
+    
+    when 0..39 then
+      text_with_low_target
+    when 61..100 then
+      text_with_high_target
+    end
+  end
+
+  
+  def text_with_low_target
+       if @target_score < 40
+         if @applicant_score < @target_score
+           return INSIGHTS[:target_low_applicant_more_underdeveloped_text]
+         elsif @applicant_score > @target_score
+           return INSIGHTS[:target_low_applicant_less_underdeveloped_text]
+         else
+           return INSIGHTS[:target_low_applicant_less_underdeveloped_text]
+         end
+       elsif @target_score > 60
+         return INSIGHTS[:target_high_applicant_underdeveloped_text]
+       else
+         return INSIGHTS[:target_general_applicant_underdeveloped_text]
+       end
+  end
+  
+  def text_with_high_target
+    if @target_score < 40
+      return INSIGHTS[:target_low_applicant_overdeveloped_text]
+    elsif @target_score > 60
+      if @applicant_score > @target_score
+        return INSIGHTS[:target_high_applicant_more_overdeveloped_text]
+      elsif @applicant_score < @target_score
+        return INSIGHTS[:target_high_applicant_less_overdeveloped_text]
+      else
+        return INSIGHTS[:target_high_applicant_less_overdeveloped_text]
+      end
+    else
+      return INSIGHTS[:target_general_applicant_overdeveloped_text]
+    end
+  end #def
+end #class
